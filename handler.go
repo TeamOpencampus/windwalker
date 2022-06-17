6b953cd6fdf75df38d3f90e2e14d60f9a94626a5
Asim Bera <asimbera@outlook.in>
2022-06-17 19:36:34 +0530

    Update mapping handler functions to its relative route

--- handler.go
+++ handler.go
@@ -2,161 +2,39 @@ package main
 
 import (
 	"errors"
-	"net/http"
-	"time"
-	"windwalker/models"
-
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
 
 func (s *Server) HealthCheck(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-		"data":   "All systems operational.",
-	})
+	NewSuccessResponse(ctx, "all systems operational")
 }
 
-func (s *Server) Login(ctx *gin.Context) {
-	db := GetDatabase(ctx)
-	// validate fields
-	var data struct {
-		Email    string `json:"email" binding:"required,email"`
-		Password string `json:"password" binding:"required"`
-	}
-	if err := ctx.ShouldBindJSON(&data); err != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   err.Error(),
-		})
-		return
-	}
-
-	// fetch user
-	var user models.User
-	if err := db.Where("email = ?", data.Email).First(&user).Error; err != nil {
-		msg := "Unknown error occured."
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			msg = "Account does not exists."
-		}
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   msg,
-		})
-		return
-	}
-
-	// check password
-	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(data.Password)); err != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   "Password does not match.",
-		})
-		return
-	}
-
-	// all ok, return jwt
-	sessionToken, err := GetToken(user.ID, user.Role, time.Now().Add(time.Hour*10))
-	// refreshToken, err := GetToken(user.ID, time.Now().Add(time.Hour*24*10))
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"status": "error",
-			"data":   "Failed to generate token.",
-		})
-		return
-	}
-
-	ctx.SetCookie("token", sessionToken, 60*60*10, "/", "localhost", false, false)
-	// ctx.SetCookie("refresh_token", refreshToken, 60*60*10, "/", "localhost", false, false)
-	ctx.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-		"data":   "Successfully signed in.",
-	})
-}
-func (s *Server) Register(ctx *gin.Context) {
-	db := GetDatabase(ctx)
-	// validate fields
-	var data struct {
-		Email    string `json:"email" binding:"required,email"`
-		Password string `json:"password" binding:"required"`
-	}
-	if err := ctx.ShouldBindJSON(&data); err != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   err.Error(),
-		})
-		return
-	}
-
-	// check email uniqueness
-	var count int64
-	if err := db.Model(&models.User{}).Where("email = ?", data.Email).Count(&count).Error; err != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   err.Error(),
-		})
-		return
-	}
-	if count != 0 {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   "An account already exists with this email.",
-		})
-		return
-	}
-
-	// create user
-	hash, err := bcrypt.GenerateFromPassword([]byte(data.Password), bcrypt.DefaultCost)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"status": "error",
-			"data":   "Failed to hash password.",
-		})
-		return
-	}
-
-	user := models.User{Email: data.Email, PasswordHash: string(hash)}
-	tx := db.Create(&user)
-	if tx.Error != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{
-			"status": "error",
-			"data":   "Failed to create account.",
-		})
-		return
-	}
-
-	// all ok, return jwt
-	sessionToken, err := GetToken(user.ID, user.Role, time.Now().Add(time.Hour*10))
-	// refreshToken, err := GetToken(user.ID, time.Now().Add(time.Hour*24*10))
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"status": "error",
-			"data":   "Failed to generate token.",
-		})
-		return
-	}
-
-	ctx.SetCookie("token", sessionToken, 60*60*10, "/", "localhost", false, false)
-	// ctx.SetCookie("refresh_token", refreshToken, 60*60*10, "/", "localhost", false, false)
-	ctx.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-		"data":   "Successfully registered.",
-	})
+func (s *Server) NoRoute(ctx *gin.Context) {
+	ctx.Abort()
+	_ = ctx.Error(errors.New("router/no-route"))
 }
 
-func (s *Server) Logout(ctx *gin.Context) {
-	ctx.SetCookie("token", "", 0, "/", "localhost", false, false)
-	ctx.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-		"data":   "You have been logged out successfully.",
-	})
+func (s *Server) NoMethod(ctx *gin.Context) {
+	ctx.Abort()
+	_ = ctx.Error(errors.New("router/no-method"))
 }
 
-func (s *Server) Verify(ctx *gin.Context) {
+// Common Handlers
+
+func (s *Server) GetOnboarding(ctx *gin.Context) {
 
 }
 
-func (s *Server) Reset(ctx *gin.Context) {
+// Handler for /secure/college routes
+
+func (s *Server) GetStudents(ctx *gin.Context) {
+
+}
+
+func (s *Server) GetCompanies(ctx *gin.Context) {
+
+}
+
+func (s *Server) GetPosts(ctx *gin.Context) {
 
 }
--- router.go
+++ router.go
@@ -1,7 +1,12 @@
 package main
 
+import "windwalker/middleware"
+
 func (s *Server) Routes() {
 	router := s.engine
+
+	router.NoRoute(s.NoRoute)
+	router.NoMethod(s.NoMethod)
 
 	v1 := router.Group("/v1")
 	{
@@ -9,35 +14,40 @@ func (s *Server) Routes() {
 		v1.POST("/login", s.Login)
 		v1.POST("/register", s.Register)
 		v1.POST("/reset")
-		secure := v1.Group("/secure", WithAuthentication())
+		secure := v1.Group("/secure", middleware.WithAuthentication("secret_key"))
 		{
 			secure.POST("/logout", s.Logout)
-			secure.GET("/onboarding")
-			secure.POST("/verify")
+			secure.GET("/onboarding", s.GetOnboarding)
+			secure.POST("/verify", s.VerifyEmail)
 
 			profile := secure.Group("/profile")
 			{
-				profile.GET("/")                // combined profile
-				profile.POST("/basic")          // create basic profile
-				profile.PUT("/basic")           // update basic profile
-				profile.POST("/academic")       // create new academic profile entry
-				profile.PUT("/academic/:id")    // update existing academic profile entry
-				profile.DELETE("/academic/:id") // delete existing academic profile entry
-				profile.POST("/work")           // create new work profile entry
-				profile.PUT("/work/:id")        // update existing work profile entry
-				profile.DELETE("/work/:id")     // delete existing work profile entry
+				profile.GET("/", s.GetProfile)                           // combined profile
+				profile.POST("/basic", s.CreateBasicProfile)             // create and update basic profile
+				profile.POST("/academic", s.CreateAcademicProfile)       // create new academic profile entry
+				profile.PUT("/academic/:id", s.UpdateAcademicProfile)    // update existing academic profile entry
+				profile.DELETE("/academic/:id", s.DeleteAcademicProfile) // delete existing academic profile entry
+				profile.POST("/work", s.CreateWorkProfile)               // create new work profile entry
+				profile.PUT("/work/:id", s.UpdateWorkProfile)            // update existing work profile entry
+				profile.DELETE("/work/:id", s.DeleteWorkProfile)         // delete existing work profile entry
 			}
+
 			feed := secure.Group("/feed")
 			{
 				feed.GET("/")    // get latest feed
 				feed.GET("/:id") // get feed item detatils
 			}
 
-			college := secure.Group("/college", WithAuthorization("college"))
+			college := secure.Group("/college", middleware.WithAuthorization("college"))
 			{
-				college.GET("/students")  // get list of student
-				college.GET("/companies") // get list of companies
-				college.GET("/posts")     // get list of posts
+				college.GET("/students", s.GetStudents)   // get list of student
+				college.GET("/companies", s.GetCompanies) // get list of companies
+				college.GET("/posts", s.GetPosts)         // get list of posts
+
+				// profile := college.Group("/profile")
+				{
+
+				}
 			}
 		}
 	}
